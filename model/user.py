5d7149defcec4829c21b915eb0b0879d9a6b050f
A-Siam <abdullah.khaled.kamal0@gmail.com>
2020-02-20 15:46:28 +0200

    edit names and use conventions

--- model/user.py
+++ model/user.py
@@ -2,8 +2,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer
 Base = declarative_base()
 
-class user(Base):
-    __tablename__ = 'USERS'
+class User(Base):
+    __tablename__ = 'USER_CAR'
     id = Column(Integer,primary_key=True,
                 unique=True,
                 autoincrement=True)
